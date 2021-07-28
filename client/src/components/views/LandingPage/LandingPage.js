aa731a7c0e16512f7c31e4389b7865768e118d01
donghyun <dawnme1228@naver.com>
2021-07-28 17:44:46 +0900

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
-  useEffect(() => {
-    axios.get('/api/hello').then((response) => {
-      console.log(response);
+function LandingPage(props) {
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        return props.history.push('/login');
+      } else {
+        return alert('로그아웃에 실패했습니다.');
+      }
     });
-  }, []);
+  };
 
   return (
     <div
@@ -18,7 +22,8 @@ function LandingPage() {
         height: '100vh',
       }}
     >
-      LandingPage
+      <h2>LandingPage</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
